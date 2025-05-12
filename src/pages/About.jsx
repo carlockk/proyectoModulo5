7fd3cbe112f4e31e3d2fc0f0d4f1493dd5f91498
carlockk <carlos.virtualdesk@gmail.com>
2025-05-12 15:22:46 -0400

    About.jsx
    Se agregó descripción en about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,8 +8,13 @@ export default function About() {
         Sobre Nosotros
       </Typography>
       <Typography variant="body1">
-        Somos una tienda comprometida con ofrecer los mejores productos.
-        Nos dedicamos a entregar una excelente experiencia a nuestros clientes.
+        Coffee Waffles nace en septiembre de 2016 y se ejecutó como tal con su inauguración en diciembre de 2016.
+Somos una pareja de jóvenes que se forma producto de la necesidad de querer emprender y dejar nuestros trabajos para vivir una nueva aventura.
+Desde ahí en adelante, solo hemos trabajado para lograr mejorar día a día nuestros productos y servicios! manteniendo la calidad en relación al precio!.
+Con el tiempo hemos adaptado el servicio a las necesidades actuales, y hemos creado productos nuevos que tanto nosotros como el resto esperamos que sea de su total agrado!
+
+Siempre innovando y probando nuevas recetas trataremos de hacer lo mejor posible por y para ustedes!.
+Coffee Waffles todos los derechos reservados del presente año!
       </Typography>
     </Container>
   );
